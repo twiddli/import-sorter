3b39381703a188f1c8599c00787280ab3d7f6c10
Twiddly <pew@pewpew.moe>
2022-02-18 21:11:23 +0100

    add new setting to ignore unused imports from specific modules;

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,38 @@
-import * as testRunner from 'vscode/lib/testrunner';
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
-module.exports = testRunner;
+import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
+
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: "tdd",
+  });
+  mocha.useColors(true);
+
+  const testsRoot = path.resolve(__dirname, "../..");
+
+  return new Promise((c, e) => {
+    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
+
+      // Add files to the test suite
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        e(err);
+      }
+    });
+  });
+}
